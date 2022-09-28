13e22bde8c04ed32e75c09f05762473fc1322dd4
Topstail <chn740932867@163.com>
2022-09-28 12:01:13 +0800

    2209281201

--- src/test01.go
+++ src/test01.go
@@ -1,11 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("Hi, Charles")
-	var arr [4]int = [4]int{0, 1, 2, 3}
-	fmt.Printf("%d", arr[1:4])
+	// fmt.Println("Hi, Charles")
+	// var arr [4]int = [4]int{0, 1, 2, 3}
+	// fmt.Printf("%d", arr[1:4])
+	var m1 map[int]string = map[int]string{}
+	m1[1] = "Tom"
+	println(m1[1])
+	m2 := map[int]string{1: "Tom", 2: "Lily"}
+	m2[0] = "John"
+	println(m2[1])
+	m3 := make(map[int]string)
+	m3[1] = "Tom"
+	println(m3[3])
+
+	println("开始循环遍历")
+	for k, v := range m2 {
+		println(k, "--->", v)
+	}
+
+	println("开始循环遍历key")
+	for k := range m2 {
+		println(k, "--->")
+	}
+
+	println("开始循环遍历value")
+	for _, v := range m2 {
+		println("--->", v)
+	}
 }
